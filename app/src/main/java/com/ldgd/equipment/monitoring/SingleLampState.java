22ab6d21a52052aec89a5766f23a51d63502f73b
liu <602694077@qq.com>
2017-08-31 17:03:26 +0800

    Initial commit

---
+++ app/src/main/java/com/ldgd/equipment/monitoring/SingleLampState.java
@@ -0,0 +1,13 @@
+package com.ldgd.equipment.monitoring;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SingleLampState extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_lamp_state);
+    }
+}
