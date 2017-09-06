dc8e80428b6b15290590f6f5dae9a1961365dc45
liu <602694077@qq.com>
2017-09-06 16:47:58 +0800

    2017年9月6日 16:47:52

--- app/src/main/java/com/ldgd/equipment/monitoring/activity/MainActivity.java
+++ app/src/main/java/com/ldgd/equipment/monitoring/activity/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this,SingleLampState.class);
+        Intent intent = new Intent(this,SingleLampStateActivity.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/ldgd/equipment/monitoring/activity/SingleLampStateActivity.java
@@ -0,0 +1,161 @@
+package com.ldgd.equipment.monitoring.activity;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.XLabels;
+import com.github.mikephil.charting.utils.YLabels;
+import com.ldgd.equipment.monitoring.R;
+import com.ldgd.equipment.monitoring.util.TimeUtils;
+
+import java.util.ArrayList;
+
+public class SingleLampStateActivity extends Activity {
+
+    private LineChart mChart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_single_lamp_state);
+
+
+        initView();
+
+        initChart1();
+
+    }
+
+    private void initView() {
+        mChart = (LineChart) findViewById(R.id.chart1);
+
+    }
+
+    private void initChart1() {
+        // if enabled, the chart will always start at zero on the y-axis
+        mChart.setStartAtZero(true);
+
+        // 双击缩放
+        mChart.setDoubleTapToZoomEnabled(false);
+
+        // disable the drawing of values into the chart
+        mChart.setDrawYValues(true);
+        mChart.setValueTextColor(Color.WHITE);
+        // 显示边界
+        mChart.setDrawBorder(false);
+        // 显示 图例
+        mChart.setDrawLegend(false);
+       //  mChart.setStartAtZero(false);
+
+        // no description text
+        mChart.setDescription("");
+        //   mChart.setUnit(" $");
+
+        // 设置点击value的时候，是否高亮显示
+        mChart.setHighlightEnabled(true);
+
+        // 设置是否可以触摸
+        mChart.setTouchEnabled(true);
+
+        // enable scaling and dragging 缩放与拖动
+        mChart.setDragEnabled(true);
+
+        // if disabled, scaling can be done on x- and y-axis separately
+        mChart.setPinchZoom(true);
+
+        mChart.setDrawGridBackground(false);
+        mChart.setDrawVerticalGrid(false);
+
+        // 网格
+        mChart.setDrawHorizontalGrid(true);
+        mChart.setDrawVerticalGrid(true);
+
+        // 设置自定义字体
+        Typeface tf = Typeface.createFromAsset(getAssets(), "OpenSans-Regular.ttf");
+        mChart.setValueTypeface(tf);
+
+
+        XLabels xl = mChart.getXLabels();
+        xl.setPosition(XLabels.XLabelPosition.BOTTOM);
+        xl.setCenterXLabelText(true);
+        xl.setTextColor(Color.WHITE);
+        xl.setTypeface(tf);
+
+
+        YLabels y = mChart.getYLabels();
+        y.setTextColor(Color.WHITE);
+        y.setTypeface(tf);
+        y.setLabelCount(5);
+
+
+
+        // add data
+        setData1(13, 0.55f);
+
+        // 动画
+        mChart.animateXY(2000, 2000);
+
+        // dont forget to refresh the drawing
+        mChart.invalidate();
+    }
+
+
+
+    private void setData1(int count, float range) {
+
+   /*     ArrayList<String> xVals = new ArrayList<String>();
+        for (int i = 0; i < count; i++) {
+            xVals.add((1990 +i) + "");
+        }*/
+
+        long currentTime = System.currentTimeMillis();
+        ArrayList<String> xVals = new ArrayList<String>();
+        for (int i = 0; i < count; i++) {
+            xVals.add(TimeUtils.dateToString(currentTime, TimeUtils.dateFormat_day));
+            currentTime -= (3 * 60 * 60 * 1000);
+        }
+
+        ArrayList<Entry> vals1 = new ArrayList<Entry>();
+
+        for (int i = 0; i < count; i++) {
+            float mult = (range + 1);
+            float val = (float) (Math.random() * mult);// + (float)
+            // ((mult *
+            // 0.1) / 10);
+            vals1.add(new Entry(val, i));
+        }
+
+        // create a dataset and give it a type
+        LineDataSet set1 = new LineDataSet(vals1, "DataSet 1");
+        set1.setDrawCubic(true);
+        set1.setCubicIntensity(0.2f);
+        set1.setDrawFilled(true);
+        set1.setDrawCircles(false);
+        set1.setLineWidth(2f);
+        set1.setCircleSize(5f);
+        set1.setHighLightColor(Color.rgb(244, 117, 117));
+        //  set1.setColor(Color.rgb(104, 241, 175));
+       //set1.setColor(Color.rgb(208,240,240));
+        set1.setColor(Color.rgb(104, 241, 175));
+
+
+        ArrayList<LineDataSet> dataSets = new ArrayList<LineDataSet>();
+        dataSets.add(set1);
+
+        // create a data object with the datasets
+        LineData data = new LineData(xVals, dataSets);
+
+
+        // set data
+        mChart.setData(data);
+    }
+}
